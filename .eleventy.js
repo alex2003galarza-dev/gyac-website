89889709f0927a8143d26f36ca8d8607c6ccbe98
alex2003galarza-dev <alex.2003galarza@gmail.com>
2026-05-06 11:05:55 -0500

    Force copy config.yml in build

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+  eleventyConfig.addPassthroughCopy("src/admin/index.html");
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.setTemplateFormats(["njk", "html", "md"]);
   return {
-    dir: { input: "src", output: "_site" }
+    dir: { input: "src", output: "_site/src" }
   };
 };
