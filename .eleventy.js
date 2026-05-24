ae972f32b2157789347ef2e4b89a619c26d1ebfd
alex2003galarza-dev <alex.2003galarza@gmail.com>
2026-05-24 15:18:38 -0700

    Delete .eleventy.js

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
-  eleventyConfig.addPassthroughCopy("src/admin/index.html");
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.setTemplateFormats(["njk", "html", "md"]);
-  return {
-    dir: { input: "src", output: "_site/src" }
-  };
-};
